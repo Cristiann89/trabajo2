7af2013d976c1ecbb9d5c14e425ca4880de683a9
cristianD1989 <cristiandiaz89@outlook.com>
2025-04-23 02:54:01 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,123 @@
+import Boton from "./components/Boton"
+
 import "./App.css";
 
-import Boton from "./components/Boton";
+
 
 function App(){
 
   const manejarClickNumeros = (e)=> {
-    console.log(e.target.textContext)
+    const boton = e.target
+    console.log(boton.textContent)
+    let display = document.getElementById("display")
+    display.value = display.value + boton.textContent
+  }
+
+  const calcular = () =>{
+    let display = document.getElementById("display")
+    display.value = eval(display.value)
+  }
+
+  const clear = () => {
+    let display = document.getElementById("display")
+    display.value = ""
   }
 
   return (
+
     <div className="contenedor">
       <div className="pad">
+        <input className="display" type="text"  id = "display"/>
       <Boton 
       texto="1"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
     texto="2"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
       texto="3"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
       texto="+"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros}
       tipo = "operacion"
       />
 
 
       <Boton 
       texto="4"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
     texto="5"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros}
       tipo = "normal"
       />
       <Boton 
       texto="6"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
       texto="-"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "operacion"
       />
 
 
      <Boton 
       texto="7"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros}
       tipo = "normal"
       />
       <Boton 
     texto="8"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
       texto="9"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros}
       tipo = "normal"
       />
       <Boton 
       texto="*"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "operacion"
       />
 
       <Boton 
       texto="="
-      operacion="manejarClickNumeros" 
+      operacion={calcular} 
       tipo = "normal"
       />
       <Boton 
       texto="0"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros}
       tipo = "normal"
       />
       <Boton 
       texto="."
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "normal"
       />
       <Boton 
       texto="/"
-      operacion="manejarClickNumeros" 
+      operacion={manejarClickNumeros} 
       tipo = "operacion"
+      />
+
+      <Boton 
+      texto="Clear"
+      operacion={clear}
+      tipo = "clear"
       />
       </div>
     </div>
